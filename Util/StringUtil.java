ecc8e22d97b9634d73882146e878435921ec1d1e
Kirill Chechetin <kirya.chechetin.90@mail.ru>
2026-06-22 19:22:03 +0500

    Package

---
+++ Util/StringUtil.java
@@ -0,0 +1,33 @@
+package Util;
+
+public class StringUtil {
+    public static void main(String[] args) {
+        String str = "J@va the be$t!123";
+        System.out.println(str);
+        System.out.println(reverse(str));
+        System.out.print("package");
+    }
+
+    public static String reverse(String str) {
+        if (str == null || str.isEmpty()) {
+            return "";
+        }
+        int start = 0;
+        int end = str.length() - 1;
+        char[] output = str.toCharArray();
+        while (start < end) {
+            if (!Character.isLetter(output[start])) {
+                start++;
+            } else if (!Character.isLetter(output[end])) {
+                end--;
+            } else {
+                char temp = output[start];
+                output[start] = output[end];
+                output[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        return new String(output);
+    }
+}
